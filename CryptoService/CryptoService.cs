603eea24d4ea68cbece3a3ffae14943af5aa6285
Peter Steele <steelep@mail.nmc.edu>
2019-10-30 13:28:12 -0400

    Add project files.

---
+++ CryptoService/CryptoService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace CryptoService
+{
+    public static class CryptoService
+    {
+        private const int SaltByteSize = 256;
+        private const int HashByteSize = 256;
+        private const int IterationCount = 26531;
+
+        public static byte[] ComputeHash(string password, byte[] salt, int iterations = 26531, int hashByteSize = 256)
+        {
+            using (Rfc2898DeriveBytes rfc2898DeriveBytes = new Rfc2898DeriveBytes(password, salt))
+            {
+                rfc2898DeriveBytes.IterationCount = iterations;
+                return rfc2898DeriveBytes.GetBytes(hashByteSize);
+            }
+        }
+
+        public static byte[] GenerateSalt(int saltByteSize = 256)
+        {
+            using (RNGCryptoServiceProvider cryptoServiceProvider = new RNGCryptoServiceProvider())
+            {
+                byte[] data = new byte[saltByteSize];
+                cryptoServiceProvider.GetBytes(data);
+                return data;
+            }
+        }
+
+        public static bool IsValidPassword(string password, string storedSalt, string passHash)
+        {
+            byte[] salt = Convert.FromBase64String(storedSalt);
+            byte[] hash = CryptoService.ComputeHash(password, salt, 26531, 256);
+            byte[] numArray = Convert.FromBase64String(passHash);
+            if (numArray.Length != hash.Length)
+                return false;
+            for (int index = 0; index < hash.Length; ++index)
+            {
+                if ((int)hash[index] != (int)numArray[index])
+                    return false;
+            }
+            return true;
+        }
+    }
+
+}
---
+++ CryptoService/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CryptoService")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CryptoService")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("30d0c802-a831-4df8-9a7b-e0f4dbcf27bd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
